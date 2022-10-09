a84434c84cf8314efa144713e9dbee9bce37e368
v0xie <28695009+v0xie@users.noreply.github.com>
2022-10-09 04:00:03 -0700

    add note about modiifers

--- BakeActionsToShapekeys.py
+++ BakeActionsToShapekeys.py
@@ -13,6 +13,9 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Modifiers must have all 4 of the little boxes enabled
+# Show on Cage, Edit mode, Realtime, Render
+
 import time
 import bpy
 D = bpy.data
